b1686ec5d4bedf463462edeb2a0440502a3fcd9a
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-05-27 00:26:46 -0300

    configure eslint with prettier with format on save

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      jest: true
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
   },
   settings: {
     react: {
